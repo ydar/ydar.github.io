70adb9ba14f6e34cd84f28d8a1bf784bd8d77803
ydar <mete.tuefekci@live.de>
2014-10-13 18:45:55 +0200

    dat colors

--- js/my.js
+++ js/my.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
         anchors: ['home', 'about', 'skillz', 'd16174l-4r7z', 'prepress', 'contact'],
-        sectionsColor: ['#F5F0F0', '#FF6447', '#E8BF69', '#D9FF56', '#43E88F', '#91E0FF'],
+        sectionsColor: ['#F5F0F0', '#c41411', '#f9a825', '#D9FF56', '#43E88F', '#91E0FF'],
         slidesNavigation: true,
         css3: true,
         scrollingSpeed: 100,
