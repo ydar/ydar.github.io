812456393ac59da3e298b07acf9ae1fc2ade1f97
ydar <mete.tuefekci@live.de>
2014-10-13 11:25:07 +0200

    update

--- js/my.js
+++ js/my.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#fullpage').fullpage({
         anchors: ['home', 'about', 'skillz', 'd16174l-4r7z', 'prepress', 'contact'],
         sectionsColor: ['#F5F0F0', '#FF6447', '#E8BF69', '#D9FF56', '#43E88F', '#91E0FF'],
@@ -6,7 +6,7 @@ $(document).ready(function() {
         css3: true,
         scrollingSpeed: 100,
         menu: '#menu',
-        fixedElements: '#filldatgap, #filldatdeg',
+        fixedElements: '#filldatgap, #menu',
         autoScrolling: true
         
     });
