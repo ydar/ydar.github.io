8ddfd50ce700e121825620449457acc99de2d918
ydar <mete.tuefekci@live.de>
2014-10-13 19:02:04 +0200

    more stuff

--- js/my.js
+++ js/my.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
         anchors: ['home', 'about', 'skillz', 'd16174l-4r7z', 'prepress', 'contact'],
-        sectionsColor: ['#F5F0F0', '#c41411', '#f9a825', '#D9FF56', '#43E88F', '#91E0FF'],
+        sectionsColor: ['#efebe9', '#c41411', '#ef6c00', '#aeea00', '#1de9b6', '#0277bd'],
         slidesNavigation: true,
         css3: true,
         scrollingSpeed: 100,
