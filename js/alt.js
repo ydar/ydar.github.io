1858625d85c2a750bb53cb6d640ab184270f7bb7
ydar <mete.tuefekci@live.de>
2014-10-14 18:04:54 +0200

    umbenennung

--- js/alt.js
+++ js/alt.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
         anchors: ['home', 'about', 'skillz', 'd16174l-4r7z', 'prepress', 'contact'],
-        sectionsColor: ['#fafafa', '#c41411', '#ef6c00', '#aeea00', '#1de9b6', '#0277bd'],
+        sectionsColor: ['#efebe9', '#c41411', '#ef6c00', '#aeea00', '#1de9b6', '#0277bd'],
         slidesNavigation: true,
         css3: true,
         scrollingSpeed: 100,
